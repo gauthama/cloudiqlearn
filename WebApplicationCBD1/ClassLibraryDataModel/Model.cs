0a06954179ade7c6ad2372c1f9b2e8bea781ddf7
gauthama <gauthama.p@gmail.com>
2018-11-09 14:29:28 +0530

    init

---
+++ WebApplicationCBD1/ClassLibraryDataModel/Model.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ClassLibraryDataModel
+{
+    public class Employee
+    {
+        public readonly int id;
+        public string nationalId;
+        public string loginId;
+
+        public Employee(int id, string nationalId, string loginId)
+        {
+            this.id = id;
+            this.nationalId = nationalId;
+            this.loginId = loginId;
+        }
+    }
+}
