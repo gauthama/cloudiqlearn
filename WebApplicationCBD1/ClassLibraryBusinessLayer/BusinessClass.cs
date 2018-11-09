c4befdbf83d669eb024f4a4bc13cdb923515b213
gauthama <gauthama.p@gmail.com>
2018-11-09 14:34:01 +0530

    init

---
+++ WebApplicationCBD1/ClassLibraryBusinessLayer/BusinessClass.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using ClassLibraryDataLayer;
+using ClassLibraryDataModel;
+
+namespace ClassLibraryBusinessLayer
+{
+    public class BusinessClass
+    {
+        public static List<Employee> GetAll(string connectionString)
+        {
+            return MyDAL.GetAll(connectionString);
+        }
+
+        public static Employee GetById(string connectionString, int id)
+        {
+            return MyDAL.GetById(connectionString, id);
+        }
+ 
+        public static void Post(string connectionString, string value)
+        {
+        }
+
+        public static void Update(string connectionString, int id, Employee emp)
+        {
+            MyDAL.Update(connectionString, id, emp);
+        }
+
+        public static void Delete(string connectionString, int id)
+        {
+        }
+    }
+}
